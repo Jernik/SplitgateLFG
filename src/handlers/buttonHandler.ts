a54545dfc8c4db0740a0620196153a2cc1837ce5
Luke <luke.miller.drss@gmail.com>
2022-01-25 22:31:20 -0600

    added commentary

--- src/handlers/buttonHandler.ts
+++ src/handlers/buttonHandler.ts
@@ -2,7 +2,10 @@ import { MessageComponentInteraction } from "discord.js";
 import { VoiceManager } from "./VoiceHandler";
 
 let buttonHandler = (interaction : MessageComponentInteraction, voiceManager:VoiceManager)=>{
-    interaction.reply("OW YOU HIT MY HEAD");
+	interaction.reply("OW YOU HIT MY HEAD");
+	//todo find the member that clicked this
+	//todo add voicemanager method to move member into party
+	//todo (in manager) find the channel that the button corresponds to, move memeber into party
 }
 
 export {buttonHandler};
