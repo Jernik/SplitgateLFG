14319f9542626137bcb453c8b61e429d72f967f1
Luke <luke.miller.drss@gmail.com>
2022-01-26 21:39:51 -0600

    move member to existing party

--- src/commands/casual.ts
+++ src/commands/casual.ts
@@ -10,7 +10,7 @@ let casual = {
 		.addStringOption((option) =>
 			option.setName("elo").setDescription("elo of the party leader")
 		),
-	async execute(interaction: CommandInteraction, voiceHandler: VoiceManager) {
+	async execute(interaction: CommandInteraction, voiceManager: VoiceManager) {
 		const elo = interaction.options.get("elo");
 		interaction.reply({
 			content: `you created a casual party with elo ${elo.value}`,
@@ -18,7 +18,7 @@ let casual = {
 		});
 		//get GuildMember object from interaction
 		if (interaction.member instanceof GuildMember) {
-			voiceHandler.createParty(interaction.member);
+			voiceManager.createParty(interaction.member);
 		} else {
 			//todo figure out how to get the member object from the APIGuildMember object ig
 		}
